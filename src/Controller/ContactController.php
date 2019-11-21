c7e59bb1f99963bf31d077dc5447193df7ddef41
Maxim Kravets <max@deovr.com>
2019-11-21 13:31:43 +0200

    Adds contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
@@ -12,8 +14,40 @@ class ContactController extends AbstractController
      */
     public function index()
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        return $this->render('contact/index.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @param \Swift_Mailer $mailer
+     *
+     * @Route("/send", name="send")
+     *
+     * @return RedirectResponse
+     */
+    public function send(Request $request, \Swift_Mailer $mailer)
+    {
+        $name = $request->get('name');
+        $from = $request->get('email');
+        $subject = $request->get('subject');
+        $message = $request->get('message');
+
+        $message = (new \Swift_Message($subject))
+            ->setFrom([$from => $name])
+            ->setTo('kravets.photographer@gmail.com')
+            ->setBody(
+                $message,
+                'text/html'
+            );
+
+        $result = $mailer->send($message);
+
+        if ($result) {
+            $this->addFlash('success', 'Your mail successfully sent!');
+        } else {
+            $this->addFlash('danger', 'Oops, something went wrong:( You can contact me at the contacts listed below');
+        }
+
+        return $this->redirectToRoute('contact');
     }
 }
